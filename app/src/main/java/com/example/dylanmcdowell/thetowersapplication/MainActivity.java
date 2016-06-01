9ec19c57f8e56faed189cca980687e14f53deede
SamCiavardini <sam.ciavardini@gmail.com>
2016-06-01 10:19:29 -0600

    What up

--- app/src/main/java/com/example/dylanmcdowell/thetowersapplication/MainActivity.java
+++ app/src/main/java/com/example/dylanmcdowell/thetowersapplication/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //suh dudes
+        // suh dudes
         // Hey y'all this is coolio.
     }
 }
