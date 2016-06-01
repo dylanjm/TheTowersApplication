f020b3366f5e94a49d4fb7979ff3e1df3e5e1112
Hysrok <doom_tew_u@yahoo.com>
2016-06-01 09:58:02 -0600

    Here ya go guys.

--- app/src/main/java/com/example/dylanmcdowell/thetowersapplication/MainActivity.java
+++ app/src/main/java/com/example/dylanmcdowell/thetowersapplication/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //suh dudes
+        // Hey y'all this is coolio.
     }
 }
