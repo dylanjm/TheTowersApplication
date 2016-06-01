2c4598bae48d89dff2a58ba2a5205569d0e070a6
Hysrok <doom_tew_u@yahoo.com>
2016-06-01 11:49:13 -0600

    Here ya go guys.

--- app/src/main/java/com/example/dylanmcdowell/thetowersapplication/MainActivity.java
+++ app/src/main/java/com/example/dylanmcdowell/thetowersapplication/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         //All is good in the hood bruh
         System.out.println("GET BENT!");
         // Howdy y'all!!!!!!!  I like GitHub.
-        //ayyy lmao
-        /// fhoaghrhgoagosgd;;agsiogj
+        // ayyy lmao
+        // When I wake up inn the morgen Gutten Tag Frauline
     }
 }
