f37743f396e4964811bdba33d2b973be0afc4de3
Dylan McDowell <dylan.mcdowell226@gmail.com>
2016-06-06 13:26:46 -0600

    Added functionality to Sign In Activity Buttons - Formated Manifest.xml

--- app/src/main/java/com/example/dylanmcdowell/thetowersapplication/SignInActivity.java
+++ app/src/main/java/com/example/dylanmcdowell/thetowersapplication/SignInActivity.java
@@ -1,13 +1,37 @@
 package com.example.dylanmcdowell.thetowersapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignInActivity extends AppCompatActivity {
+    Button signIn;
+    Button createProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        //Sign In Button
+        signIn = (Button) findViewById(R.id.button);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent("android.intent.action.MAIN");
+                startActivity(intent);
+
+            }
+        });
+
+        //Create Profile Button
+        createProfile = (Button) findViewById(R.id.button2);
+        createProfile.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent intent = new Intent("android.intent.action.CREATEPROFILE");
+                startActivity(intent);
+            }
+        });
     }
 }
