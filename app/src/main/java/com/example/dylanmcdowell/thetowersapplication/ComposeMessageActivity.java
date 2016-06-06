cf3429856cbc6fc31118f5f7c6b86402b41779e1
Dylan McDowell <dylan.mcdowell226@gmail.com>
2016-06-06 12:23:25 -0600

    Added Compose Message Activity and its respective .xml file.

---
+++ app/src/main/java/com/example/dylanmcdowell/thetowersapplication/ComposeMessageActivity.java
@@ -0,0 +1,13 @@
+package com.example.dylanmcdowell.thetowersapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ComposeMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose_message);
+    }
+}
