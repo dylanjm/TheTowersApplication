cce84365ff70cb03290b61399dc709dcf1a315e5
SamCiavardini <sam.ciavardini@gmail.com>
2016-06-27 12:43:45 -0600

    added reference to recipient in compose message

--- app/src/main/java/com/example/dylanmcdowell/thetowersapplication/ComposeMessageActivity.java
+++ app/src/main/java/com/example/dylanmcdowell/thetowersapplication/ComposeMessageActivity.java
@@ -12,20 +12,23 @@ import com.google.firebase.database.FirebaseDatabase;
 public class ComposeMessageActivity extends AppCompatActivity {
     EditText topicString;
     EditText messageString;
+    EditText recipientString;
     Button send;
     String topic;
     String message;
+    //String recipient;
     private static final String FIREBASE_URL = "https://android-chat.firebaseio-demo.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose_message);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
         final DatabaseReference myRef = database.getReference("message");
 
         send = (Button) findViewById(R.id.button11);
         topicString = (EditText) findViewById(R.id.editText3);
+        recipientString = (EditText) findViewById(R.id.editText9);
         messageString = (EditText) findViewById(R.id.editText4);
         send.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -33,8 +36,10 @@ public class ComposeMessageActivity extends AppCompatActivity {
                     // Create our 'model', a Chat object
                     String subject = topicString.getText().toString();
                     String body = messageString.getText().toString();
+                    String recipient = recipientString.getText().toString();
+                    DatabaseReference myRef = database.getReference(recipient);
                     System.out.println(subject + body);
-                    Message chat = new Message(subject, body, "BILL");
+                    Message chat = new Message(subject, body, recipient);
                     // Create a new, auto-generated child of that chat location, and save our chat data there
                     myRef.push().setValue(chat);
                     try {
