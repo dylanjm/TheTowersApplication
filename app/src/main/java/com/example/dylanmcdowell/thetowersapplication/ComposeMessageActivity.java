ebd91b9dc6bbe9c60642a8accd511ebb80d226b4
SamCiavardini <sam.ciavardini@gmail.com>
2016-06-27 12:12:52 -0600

    added click listener

--- app/src/main/java/com/example/dylanmcdowell/thetowersapplication/ComposeMessageActivity.java
+++ app/src/main/java/com/example/dylanmcdowell/thetowersapplication/ComposeMessageActivity.java
@@ -2,6 +2,7 @@ package com.example.dylanmcdowell.thetowersapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -21,29 +22,30 @@ public class ComposeMessageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose_message);
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
+        final DatabaseReference myRef = database.getReference("message");
 
         send = (Button) findViewById(R.id.button11);
         topicString = (EditText) findViewById(R.id.editText3);
         messageString = (EditText) findViewById(R.id.editText4);
-
-        if (!messageString.equals("")) {
-            // Create our 'model', a Chat object
-            String subject = topicString.getText().toString();
-            String body = messageString.getText().toString();
-            Message chat = new Message(subject, body, "BILL");
-            // Create a new, auto-generated child of that chat location, and save our chat data there
-            myRef.push().setValue(chat);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        send.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                if (!messageString.equals("")) {
+                    // Create our 'model', a Chat object
+                    String subject = topicString.getText().toString();
+                    String body = messageString.getText().toString();
+                    System.out.println(subject + body);
+                    Message chat = new Message(subject, body, "BILL");
+                    // Create a new, auto-generated child of that chat location, and save our chat data there
+                    myRef.push().setValue(chat);
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    //topicString.setText("");
+                    //messageString.setText("");
+                }
             }
-            topicString.setText("");
-            messageString.setText("");
-        }
-
-
-
+        });
     }
 }
