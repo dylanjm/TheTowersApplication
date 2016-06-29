c11d3bfe4e84d0b4ed480aa7096b2800708c0f1a
SamCiavardini <sam.ciavardini@gmail.com>
2016-06-29 01:16:21 -0600

    There is now a request class, and the submit request activity works. Also added a firebase list adapter. Still trying to figure out how to use it.

--- app/src/main/java/com/example/dylanmcdowell/thetowersapplication/SubmitRequestActivity.java
+++ app/src/main/java/com/example/dylanmcdowell/thetowersapplication/SubmitRequestActivity.java
@@ -5,9 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SubmitRequestActivity extends AppCompatActivity {
 
+    EditText topicString;
+    EditText messageString;
     Button submit;
     Button cancel;
     CheckBox checkbox;
@@ -17,12 +23,30 @@ public class SubmitRequestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_submit_request);
 
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        topicString = (EditText) findViewById(R.id.editText6);
+        messageString = (EditText) findViewById(R.id.editText5);
+        checkbox = (CheckBox) findViewById(R.id.checkbox);
+
         //submit button
         submit = (Button) findViewById(R.id.button);
         submit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                finish();
+                String subject = topicString.getText().toString();
+                String body = messageString.getText().toString();
+                Boolean emergency = checkbox.isChecked();
 
+                DatabaseReference myRef = database.getReference("Requests");
+                System.out.println(subject + body);
+                Request request = new Request(subject, body, "Yo momma", emergency);
+                // Create a new, auto-generated child of that chat location, and save our chat data there
+                myRef.push().setValue(request);
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                //finish();
             }
         });
 
