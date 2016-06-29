d89924d225d70230c5a33915ed00f1afd69712fc
SamCiavardini <sam.ciavardini@gmail.com>
2016-06-29 17:39:26 -0600

    Working on reading messages from the database

--- app/src/main/java/com/example/dylanmcdowell/thetowersapplication/InboxActivity.java
+++ app/src/main/java/com/example/dylanmcdowell/thetowersapplication/InboxActivity.java
@@ -6,15 +6,38 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class InboxActivity extends AppCompatActivity {
-    Button message;
+    Button sampleMessage;
+    Message message;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inbox);
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("users").child("Sam Ciavardini").child("messages").addListenerForSingleValueEvent(
+                new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        // Get user value
+                        message = dataSnapshot.getValue(Message.class);
 
-        message = (Button) findViewById(R.id.button10);
-        message.setOnClickListener(new View.OnClickListener() {
+                        // .
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        //Log.w(TAG4, "getUser:onCancelled", databaseError.toException());
+                    }
+                });
+
+        sampleMessage = (Button) findViewById(R.id.button10);
+        sampleMessage.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 Intent intent = new Intent("android.intent.action.VIEWMESSAGE");
                 startActivity(intent);
